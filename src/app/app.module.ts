03ba1ee8b84ed2bcc9d94c66e6e53bcf53b29542
Sebastian <jsruiz7411@misena.edu.co>
2021-02-18 13:55:48 -0500

    Componentes Completos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { TablaComponent } from './dashboard/tabla/tabla.component';
+import { ListaComponent } from './dashboard/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent,DashboardComponent, NavbarComponent, FooterComponent
+    AppComponent,DashboardComponent, NavbarComponent, FooterComponent, TablaComponent, ListaComponent
   ],
   imports: [
     BrowserModule,FormsModule
